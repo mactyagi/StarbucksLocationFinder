26eb2e921b51f729185fb8c250415f2e6bfc715a
mactyagi <mactyagi@icloud.com>
2024-01-29 18:26:43 +0530

    Initial Commit

---
+++ StarbucksLocationFinder/StarbucksLocationFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  StarbucksLocationFinderApp.swift
+//  StarbucksLocationFinder
+//
+//  Created by Manu on 29/01/24.
+//
+
+import SwiftUI
+
+@main
+struct StarbucksLocationFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
